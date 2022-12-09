f71d16416d21a5305943621b2caea401a363b661
Mouhcine Daali <the.daali.mouhcine@gmail.com>
2022-12-09 23:52:09 +0100

    add custome hook

---
+++ src/common/hooks/useLocaleStorage.ts
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+
+function useLocaleStorage<T>(key: string, initialValue?: T) {
+  const [allo, setallo] = useState(null);
+  const [storedValue, setStoredValue] = useState<any>(() => {
+    if (typeof window === "undefined") {
+      return initialValue;
+    }
+    try {
+      const item = window.localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      console.log(error);
+
+      return initialValue;
+    }
+  });
+
+  useEffect(() => {
+    setallo(storedValue);
+  }, [storedValue]);
+
+  const getValue = () => {
+    return storedValue;
+  };
+  
+  const setValue = (value: T) => {
+    try {
+      let valueToStore = value instanceof Function ? value(storedValue) : value;
+      valueToStore = valueToStore === undefined ? null : valueToStore;
+      setStoredValue(valueToStore);
+      window.localStorage.setItem(key, JSON.stringify(valueToStore));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const clearValue = () => {
+    try {
+      window.localStorage.removeItem(key);
+      setStoredValue(initialValue ?? (null as unknown as T));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return { storedValue, setValue, clearValue, getValue, allo };
+}
+
+export default useLocaleStorage;
