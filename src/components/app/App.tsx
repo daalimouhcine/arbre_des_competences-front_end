b65ff435c0ae2f4f4cfff03913e5865c2cf5bb63
Mouhcine Daali <the.daali.mouhcine@gmail.com>
2022-12-12 23:39:53 +0100

    add function to check if user is exists in localestorage

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,13 +1,19 @@
-import {Route, Routes} from 'react-router-dom';
+import {Route, Routes, useNavigate} from 'react-router-dom';
+import useLocaleStorage from '../../common/hooks/useLocaleStorage';
 import LoginPage from '../../pages/auth/login.page';
 import NotFound from '../../pages/404/404.page';
 import HomePage from '../../pages/home/home.page';
 
 const App = () => {
+  const [user] = useLocaleStorage('user');
+  const navigate = useNavigate();
+
   return (
     <Routes>
       <Route path="/login" element={<LoginPage />} />
-      <Route path="/home" element={<HomePage />} />
+      <>
+        {user ? <Route path="/" element={<HomePage />} /> : navigate('/login') }
+      </>
       
       <Route path="*" element={<NotFound />} />
     </Routes>
